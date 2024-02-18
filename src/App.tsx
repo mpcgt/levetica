e76f2898f8b6e075a8317cd07f34d9fb2bd286ff
Max <max.pcgt@gmail.com>
2024-02-18 20:50:35 +0100

    Updates files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import reactLogo from './assets/react.svg'
 import Navbar from './assets/components/Navbar.tsx'
 import imagePath from './assets/levetica.svg'
-import lvtcw from './assets/lvtcw.png'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './App.css'
 import './index.css'
 
 function App() {
-  let items = ["Accueil","Alternatives","Blog","À propos"];
+  let items = ["Accueil","Outils","Articles","À propos"];
   return (
     <>
       <div>
@@ -21,7 +20,7 @@ function App() {
       <br />
     <div className='center'>
       <a href="https://github.com/mpcgt/levetica" target="_blank">
-          <img src={lvtcw} height="100" width="100" className="logo" alt="Levetica" />
+          <img src={imagePath} height="100" width="100" className="logo" alt="Levetica" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} height="100" width="100" className="logo" alt="React" />
@@ -30,10 +29,11 @@ function App() {
       </div>
       <br />
       <div className='center'>
-      <h1 className='text'>Découvrez nos recommandations des logiciels et des OS. 👨‍💻</h1>
+      <h1 className='text'>Fini les hésitations, trouve les outils adaptés à tes besoins. 👨‍💻</h1>
       <h3 className='text'>Levetica + React = ❤️</h3>
         <p className='text'>
-          Levetica est actuellement en construction...
+          <i>Levetica est actuellement en construction, ce site passe de Angular à React </i><img alt="Loading" height="24" width="24" src="./src/assets/images/loading.gif" />
+
         </p>
       </div>
     <br />
@@ -46,10 +46,7 @@ function App() {
       Ce site web est actuellement en construction, ce site sera officiellement publiée en bêta à partir du <b>29 mars à 21h <i>(repoussé à 1 mois).</i></b>
   <br />
   <br />
-    Vous avez atteint le site web de <b>Levetica</b>, un site web de nos recommandation des logiciels et des systèmes d'exploitation qui sont plus respectueux de la vie privée et qui sont adaptés selon vos besoins.
-  <br />
-  <br />
-    Je vous rappelle que pour le moment, vous ne pouvez pas accéder ce site dans votre téléphone, elle n'est pour le moment compatible uniquement sur ordinateur <i>(Windows, Mac, Linux...)</i>
+    Vous avez atteint le site web de <b>Levetica</b>, un site web de nos recommandations des logiciels, des outils et des sites web qui sont utiles selon vos besoins des développeurs.
   <br />
   <br />
     N'hésitez pas à consulter le <a href="https://github.com/mpcgt/levetica">code source</a> du site et merci à ceux qui me soutiennent en laissant une ⭐ dans mon projet. Jetez aussi un coup d'oeil dans ce site si vous voulez en savoir plus dans Levetica. 😊
@@ -59,27 +56,35 @@ function App() {
   </p>
   <br />
   <br />
+  <h2>Bonjour ! 👋🏼</h2>
   <br />
   <br />
-  <h2 className='text'>Bonjour ! 👋🏼</h2>
+      <h4><b><u>Quel est le but de ce projet ?</u></b></h4>
+  <br />
+      <p>Aider les développeurs, débutants ou expérimentés, à trouver les outils et les langages de programmation les plus adaptés à leurs besoins.</p>
+  <br />
+      <div className='center'>
+        <img alt="Dev" height="250" width="500" src="./src/assets/images/dev.png" />
+      </div>
+  <br />
+  <h4><b><u>Comment ça fonctionne ?</u></b></h4>
+  <br />
+      <p>1. <b>Pour chaque outil et langage, le site fournit une description détaillée</b>, des exemples d'utilisation, des tutoriels et des liens vers la communauté.</p>
+      <p>2. <b>Le développeur peut également consulter des articles et des guides</b> pour l'aider à choisir les meilleurs outils pour son projet.</p>
+  <br />
+    <h4><b><u>Quels avantages ?</u></b></h4>
+      <p>1. <b>Gain de temps et d'énergie :</b> le développeur n'a pas à passer des heures à rechercher les outils et les langages qui lui conviennent.</p>
+      <p>2. <b>Choix éclairé :</b> le développeur a accès à toutes les informations dont il a besoin pour prendre une décision réfléchie.</p>
+      <p>3. <b>Développement plus efficace :</b> le développeur utilise les outils et les langages les plus adaptés à ses besoins, ce qui lui permet de travailler plus rapidement et de manière plus productive.</p>
+  <br />
+  <h4><b><u>Quels public ciblés ?</u></b></h4>
+  <br />
+      <p>1. <b>Développeurs débutants</b> qui ne savent pas quels outils et langages choisir.</p>
+      <p>2. <b>Développeurs expérimentés</b> qui recherchent de nouveaux outils pour améliorer leur productivité.</p>
+      <p>3. <b>Étudiants en informatique</b> qui veulent découvrir les différents langages de programmation.</p>
   <br />
   <br />
-      <p className='text'><b>Le monde numérique est dominé par les GAFAM</b> (Google, Amazon, Facebook, Apple et Microsoft). Ces géants du web offrent des services pratiques et populaires, mais ils <b>soulèvent également des inquiétudes</b> concernant la confidentialité, la surveillance, l'éthique et la concentration du pouvoir.
-  <br />
-  <br />
-        <img alt="GAFAM" height="220" width="500" src="./src/assets/images/gafam.png" />
-  <br />
-  <br />
-      <b>C'est pourquoi j'ai créé Levetica.</b> Mon objectif est de <b>partager des informations et des réflexions sur les alternatives aux GAFAM.</b> Je souhaite encourager les gens à <b>explorer des solutions plus respectueuses de la vie privée, de l'éthique et de la diversité.</b>
-  <br />
-  <br />
-      <b>Je crois que chacun a le droit de choisir les outils numériques qu'il utilise.</b> Levetica vise à <b>donner aux gens les moyens de s'affranchir de la dépendance aux GAFAM</b> et de construire un web plus libre et plus ouvert.
-  <br />
-  <br />
-      <b>En explorant ces alternatives, vous pouvez réduire votre dépendance aux GAFAM et contribuer à un web plus diversifié et plus respectueux de vos données.</b>
-  <br />
-  <br />
-      A bientôt et merci à ceux qui me soutiennent en laissant une ⭐ dans mon projet sur <a href="https://github.com/mpcgt/levetica">GitHub</a> 😊</p>
+      A bientôt et merci à ceux qui me soutiennent en laissant une ⭐ dans mon projet sur <a href="https://github.com/mpcgt/levetica">GitHub</a> 😊
   </div>
   </>
   )
