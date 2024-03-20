00e81f74fe422ccbd061a46d13e7ae4de70b22e9
Max <max.pcgt@gmail.com>
2024-03-20 23:44:56 +0100

    Update file (possible bugs)

--- src/NewNavBar.tsx
+++ src/NewNavBar.tsx
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom';
 const NavBar = ({ links }) => {
   return (
     <nav className="navbar">
+      <img src="https://github.com/mpcgt/levetica/blob/main/src/assets/levetica.png?raw=true" alt="Levetica" width="30" height="24">Levetica</img>
       <ul>
         {links.map((link) => (
           <li key={link.to}>
