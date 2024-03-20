df689825f65f7d53435a4ee37aa4adf1ce43fd9a
Max <max.pcgt@gmail.com>
2024-03-20 17:54:36 +0100

    Updates files

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import Navbar from './assets/components/Navbar.tsx'
-import imagePath from './assets/levetica.svg'
+import NavBarNew from './assets/components/NavbarNew.tsx'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Text from './index.tsx'
 
 function App() {
-  let items = ["Accueil","Produits","Articles","À propos"];
   return (
     <>
-        <Navbar 
-        brandName='Levetica' 
-        imageSrcPath={imagePath}
-        navItems={items}
-        />
+      <NavBarNew />
       <Text />
   </>
   )
