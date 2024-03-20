c3ec8e3ae7b8b49ea8bdc69b63463bed7ed0f096
Max <max.pcgt@gmail.com>
2024-03-20 22:28:44 +0100

    Bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,19 @@ import Tools from '../src/assets/routes/pages/tools.tsx'
 import News from '../src/assets/routes/pages/news.tsx'
 import About from '../src/assets/routes/pages/about.tsx'
 import Text from './index.tsx'
+import Navbar from './assets/components/Navbar.tsx'
+import imagePath from './assets/levetica.svg'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
+  let items = ["Accueil","Produits","Articles","À propos"];
   return (
     <>
+      <Navbar 
+        brandName='Levetica' 
+        imageSrcPath={imagePath}
+        navItems={items}
+      />
     <div className='App'>
       <Routes>
         <Route path='/' element={<Home />} />
