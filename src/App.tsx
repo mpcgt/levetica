07350f45ae86db0bcd7b55c48ef24b4e957faf13
Max <max.pcgt@gmail.com>
2024-03-20 18:42:26 +0100

    Bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import NavBarNew from './assets/components/NavbarNew.tsx'
+import NavBarNew from './assets/components/Navbar_new.tsx'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Text from './index.tsx'
