111e092a433a98e182a84beee6497fa344c3967c
Max <max.pcgt@gmail.com>
2024-03-20 13:18:17 +0100

    Updates files

--- src/assets/components/Footer.tsx
+++ src/assets/components/Footer.tsx
@@ -1,4 +1,5 @@
 import { Box, Heading, Flex, List, ListItem, Link, Text } from '@chakra-ui/react';
+import { BrowserRouter as Router, Route, redirect,} from "react-router-dom";
 
 const FooterEnd = () => {
   return (
