c1eac76b23b129f3c56a32f8a8db35fa5ace4e24
Max <max.pcgt@gmail.com>
2024-03-20 18:02:40 +0100

    Bug fix

--- src/assets/routes/blog.tsx
+++ src/assets/routes/blog.tsx
@@ -1,4 +1,3 @@
-import Error404 from '../assets/404.svg'
 import './App.css'
 
 function News() {
@@ -6,10 +5,6 @@ function News() {
     <>
 <div>
       <br />
-        <br />
-        <br />
-        <br />
-        <img src={Error404} height="100" width="100" className="logo" alt="Error 404" />
         <br />
         <br />
         <h4>D'une manière ou d'une autre, vous vous êtes perdu dans les profondeurs de l'espace.</h4><a href="https://levetica.vercel.app">Retour</a>
