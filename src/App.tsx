cb21c84b739dd0546da66baca0e6a174f08fc3eb
Max <max.pcgt@gmail.com>
2024-03-20 21:41:14 +0100

    Update file

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,25 @@ import Navbar from './assets/components/Navbar.tsx'
 import imagePath from './assets/levetica.svg'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import Home from '../src/index.tsx'
+import Tools from '../src/assets/routes/pages/tools.tsx'
+import News from '../src/assets/routes/pages/news.tsx'
+import About from '../src/assets/routes/pages/about.tsx'
 import Text from './index.tsx'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   let items = ["Accueil","Produits","Articles","À propos"];
   return (
     <>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/outils' element={<Tools />} />
+        <Route path='/articles' element={<News />} />
+        <Route path='/a-propos' element={<About />} />
+      </Routes>
+    </div>
         <Navbar 
         brandName='Levetica' 
         imageSrcPath={imagePath}
