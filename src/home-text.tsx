98cb5a51cb5141c12114e0bca2292b61f2f1fc0c
Max <max.pcgt@gmail.com>
2024-03-20 20:02:12 +0100

    Bug fix

--- src/home-text.tsx
+++ src/home-text.tsx
@@ -1,15 +1,9 @@
-import Error404 from './assets/404.svg'
-import './App.css'
-
 function Home() {
   return (
     <>
 <div>
       <br />
         <br />
-        <br />
-        <br />
-        <img src={Error404} height="100" width="100" className="logo" alt="Error 404" />
         <br />
         <br />
         <h4>D'une manière ou d'une autre, vous vous êtes perdu dans les profondeurs de l'espace.</h4><a href="https://levetica.vercel.app">Retour</a>
