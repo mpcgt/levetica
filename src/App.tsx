2ea5385b54125aa5c17c852b21cce190d14326e9
Max <max.pcgt@gmail.com>
2024-03-20 18:53:15 +0100

    Bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import NavBarNew from './assets/components/Navbar_new.tsx'
+import Navbar from './assets/components/Navbar.tsx'
+import imagePath from './assets/levetica.svg'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Text from './index.tsx'
 
 function App() {
+  let items = ["Accueil","Outils","Articles","À propos"];
   return (
     <>
-      <NavBarNew />
+        <Navbar 
+        brandName='Levetica' 
+        imageSrcPath={imagePath}
+        navItems={items}
+        />
       <Text />
   </>
   )
