57fcef8ceb5f8aa589702867bb8f3dd0eb836bca
Max <max.pcgt@gmail.com>
2024-03-21 12:14:05 +0100

    Update file

--- src/NewNavBar.tsx
+++ src/NewNavBar.tsx
@@ -4,8 +4,7 @@ import imagePathWhite from '../src/assets/lvtcw.png'
 const NavBar = ({ links }) => {
   return (
     <nav className="navbar">
-        <img src={imagePathWhite} height="25" width="30" className="logo" alt="Levetica" />
-        <h3>Levetica</h3>
+        <img src={imagePathWhite} height="25" width="30" className="logo" alt="Levetica" /><h3>Levetica</h3>
       <ul>
         {links.map((link) => (
           <li key={link.to}>
