1ebd0c3259867e5cf1af8daf0b6253211a38f254
Max <max.pcgt@gmail.com>
2024-03-22 11:05:37 +0100

    Update file

--- src/assets/routes/pages/tools.tsx
+++ src/assets/routes/pages/tools.tsx
@@ -23,15 +23,17 @@ function Tools() {
       <h4>Les outils adaptés par langages de programmation sont des ressources essentielles pour les développeurs, leur permettant de travailler de manière efficace et efficiente dans un environnement de développement spécifique à chaque langage. Ces outils sont conçus pour répondre aux besoins particuliers des développeurs travaillant dans un langage spécifique, en offrant des fonctionnalités telles que la syntaxe de coloration, la complétion automatique de code, la vérification de la syntaxe, le débogage et le profilage. Ces outils permettent aux développeurs de gagner du temps, de réduire les erreurs et d'améliorer leur productivité en fournissant un environnement de développement spécialement adapté aux particularités et aux exigences de chaque langage de programmation.</h4>
     </div>
     <br />
-      <button onClick={() => navigate("/apps")}><img alt="Open-Source" height="30" width="35" src={opensource} />Applications</button>
-      <button onClick={() => navigate("/react")}><img alt="React" height="30" width="35" src={react} />React</button>
-      <button onClick={() => navigate("/angular")}><img alt="Angular" height="30" width="35" src={angular} />Angular</button>
-      <button onClick={() => navigate("/vue")}><img alt="Vue" height="30" width="35" src={vue} />Vue</button>
-      <button onClick={() => navigate("/html")}><img alt="HTML" height="30" width="35" src={html} />HTML</button>
-      <button onClick={() => navigate("/css")}><img alt="CSS" height="30" width="35" src={css} />CSS</button>
-      <button onClick={() => navigate("/js")}><img alt="JavaScript" height="30" width="35" src={js} />JavaScript</button>
-      <button onClick={() => navigate("/ts")}><img alt="TypeScript" height="30" width="35" src={ts} />TypeScript</button>
-      <button onClick={() => navigate("/reactn")}><img alt="React" height="30" width="35" src={react} />React Native</button>
+    <div className='center'>
+      <button onClick={() => navigate("/apps")}><img alt="Open-Source" height="25" width="30" src={opensource} />Applications</button>
+      <button onClick={() => navigate("/react")}><img alt="React" height="25" width="30" src={react} />React</button>
+      <button onClick={() => navigate("/angular")}><img alt="Angular" height="25" width="30" src={angular} />Angular</button>
+      <button onClick={() => navigate("/vue")}><img alt="Vue" height="25" width="30" src={vue} />Vue</button>
+      <button onClick={() => navigate("/html")}><img alt="HTML" height="25" width="30" src={html} />HTML</button>
+      <button onClick={() => navigate("/css")}><img alt="CSS" height="25" width="30" src={css} />CSS</button>
+      <button onClick={() => navigate("/js")}><img alt="JavaScript" height="25" width="30" src={js} />JavaScript</button>
+      <button onClick={() => navigate("/ts")}><img alt="TypeScript" height="25" width="30" src={ts} />TypeScript</button>
+      <button onClick={() => navigate("/reactn")}><img alt="React" height="25" width="30" src={react} />React Native</button>
+    </div>
   </div>
   </>
   )
