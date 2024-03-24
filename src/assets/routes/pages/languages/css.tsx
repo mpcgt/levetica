af3d920ebaa7679945ecfa7167c5762920156d04
Max <max.pcgt@gmail.com>
2024-03-24 23:57:12 +0100

    Bug fix

--- src/assets/routes/pages/languages/css.tsx
+++ src/assets/routes/pages/languages/css.tsx
@@ -1,12 +1,11 @@
 import '../pages.css'
-import css_png from '../others/languages/images/css.png'
 
 function CSS() {
     return (
       <>
       <div>
        <div className='center' />
-       <h1>Outils pour CSS<img alt="CSS" height="65" width="65" src={css_png} /></h1>
+       <h1>Outils pour CSS</h1>
     </div>
     </>
     )
