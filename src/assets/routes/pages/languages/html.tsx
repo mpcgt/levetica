669111afd208d620700bb4d82cf389445cdd949e
Max <max.pcgt@gmail.com>
2024-03-24 23:58:21 +0100

    Bug fix

--- src/assets/routes/pages/languages/html.tsx
+++ src/assets/routes/pages/languages/html.tsx
@@ -1,15 +1,15 @@
 import '../pages.css'
-import html_png from '../others/languages/images/html.png'
 
 function HTML() {
     return (
       <>
       <div>
        <div className='center' />
-       <h1>Outils pour HTML<img alt="HTML" height="65" width="65" src={html_png} /></h1>
+       <h1>Outils pour HTML</h1>
     </div>
     </>
     )
   }
   
+  // eslint-disable-next-line react-refresh/only-export-components
   export default HTML;
