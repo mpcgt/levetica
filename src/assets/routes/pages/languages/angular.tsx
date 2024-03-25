99af44e502f76a501c934f9672b2a2543822dfaf
Max <max.pcgt@gmail.com>
2024-03-25 10:36:02 +0100

    Updates files

--- src/assets/routes/pages/languages/angular.tsx
+++ src/assets/routes/pages/languages/angular.tsx
@@ -1,4 +1,5 @@
 import '../pages.css'
+import angular_png from '../others/languages/images/angular.png'
 
 function Angular() {
     return (
@@ -7,6 +8,23 @@ function Angular() {
        <div className='center'>
        <h1>Outils pour Angular<img alt="Angular" height="65" width="65" src='https://github.com/mpcgt/levetica/blob/main/src/assets/routes/others/languages/images/angular.png?raw=true' /></h1>
     </div>
+
+    <div className='border'>
+      <div className='margin'>
+      <div className='float'>
+    <img alt="Angular IDE" height="70" width="70" src={angular_png} /><h2><a href='https://angular.dev' target='_blank'>Angular IDE</a></h2>
+      </div>
+      <h4><i>Environnement de développement | Gratuit • Open Source</i></h4>
+      <h3>Angular IDE est un environnement de développement intégré (IDE) spécialement conçu pour le framework Angular. Il est disponible sous forme de plugin autonome, il facilite le développement en offrant des fonctionnalités spécifiques au framework</h3>
+  <br />
+      <h2><b>Les fonctionnalités de Angular IDE :</b></h2>
+      <h3><u>• Validation et débogage TypeScript ✅ :</u> Angular IDE offre une validation et un débogage avancés pour TypeScript.</h3>
+      <h3><u>• Intégration CLI 🛠️ :</u> Il est intégré avec la ligne de commande, ce qui facilite le développement.</h3>
+      <h3><u>• Modèles HTML avancés ✏️ :</u> Angular IDE fournit des modèles HTML avancés, ce qui accélère le processus de développement.</h3>
+      <h3><u>• Prise en charge d’Angular et TypeScript 💻 :</u> Il prend en charge les dernières versions d’Angular et TypeScript, ce qui garantit la compatibilité avec les dernières fonctionnalités.</h3>
+      <h3><u>• Autocomplétion du code 📝 :</u> Angular IDE offre une autocomplétion du code, ce qui permet d’écrire du code plus rapidement et avec moins d’erreurs.</h3>
+  </div>
+  </div>
     </div>
     </>
     )
--- src/assets/routes/pages/languages/react.tsx
+++ src/assets/routes/pages/languages/react.tsx
@@ -1,4 +1,5 @@
 import '../pages.css'
+import react_dev from '../images/react-dev.png'
 
 function React() {
     return (
@@ -7,6 +8,23 @@ function React() {
        <div className='center'>
        <h1>Outils pour React<img alt="React" height="65" width="65" src='https://github.com/mpcgt/levetica/blob/main/src/assets/routes/others/languages/images/react.png?raw=true' /></h1>
        </div>
+
+       <div className='border'>
+      <div className='margin'>
+      <div className='float'>
+    <img alt="React Dev" height="70" width="70" src={react_dev} /><h2><a href='https://microsoftedge.microsoft.com/addons/detail/react-developer-tools/gpphkfbcpidddadnkolkpfckpihlkkil' target='_blank'>React Developer Tools</a></h2>
+      </div>
+      <h4><i>Extensions de navigateur | Gratuit • Open Source</i></h4>
+      <h3>React Developer Tools est une extension de navigateur qui permet d’inspecter et de modifier les hiérarchies de composants React. Elle offre également des outils pour identifier les problèmes de performance et pour éditer les props et l’état des composants.</h3>
+  <br />
+      <h2><b>Les fonctionnalités de React Developer Tools :</b></h2>
+      <h3><u>• Inspection des composants 🔎 :</u> React Developer Tools permet d’inspecter les hiérarchies de composants React, ce qui facilite la compréhension de la structure de l’application.</h3>
+      <h3><u>• Modification des props et de l’état ✏️ :</u> Ces outils permettent de modifier les props et l’état des composants, ce qui est utile pour le débogage et le test.</h3>
+      <h3><u>• Identification des problèmes de performance 📈 :</u> Il offre des outils pour identifier les problèmes de performance, ce qui peut aider à optimiser le site web.</h3>
+      <h3><u>• Facilité d’apprentissage 🥸 :</u> React Developer Tools est simple à apprendre et à comprendre, ce qui le rend accessible même aux développeurs débutants.</h3>
+      <h3><u>• Compatibilité avec plusieurs navigateurs 🌍 :</u> L’extension est disponible pour plusieurs navigateurs populaires, ce qui offre une grande flexibilité.</h3>
+  </div>
+  </div>
        </div>
     </>
     )
