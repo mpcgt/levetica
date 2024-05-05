a837ebaa17f86f53de95b875931ec3f4c9743ab6
max.pcgt <max.pcgt@gmail.com>
2024-05-05 11:35:15 +0000

    Bug fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-// import ReactGA from "react-ga";
-import ReactGA from "react-ga4";
-
-ReactGA.initialize("G-C9NJF08G05");
-
-// Send pageview with a custom path
-ReactGA.send({ 
-  hitType: "pageview",
-  page: window.location.pathname,
-});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
