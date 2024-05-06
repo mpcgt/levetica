6679519fff215bda3b3d8dc54f41d9c86791fcb2
max.pcgt <max.pcgt@gmail.com>
2024-05-06 16:16:46 +0000

    Bug fix

--- src/home.tsx
+++ src/home.tsx
@@ -1,13 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import HomeText from './home-text.tsx'
-import BannerHome from './assets/components/banner.tsx'
-
 function App() {
   return (
     <>
       <HomeText />
-      <BannerHome />
   </>
   )
 }
