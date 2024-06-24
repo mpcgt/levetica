8761e01bd195f2e022389a4154f254259516d8b6
Max <max.pcgt@gmail.com>
2024-06-24 15:56:54 +0200

    Updates files - v.1.3.0-beta.10

--- src/assets/components/tools/pages/security.tsx
+++ src/assets/components/tools/pages/security.tsx
@@ -1,10 +1,46 @@
 import NavbarTools from "../NavBarTools";
+import { Link } from "react-router-dom";
 
 function Security() {
     return (
       <>
       <NavbarTools />
-        <h1>Bientôt disponible</h1>
+      <div
+        className="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5"
+        tabIndex={-1}
+        role="dialog"
+        id="modalChoice"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content rounded-3 shadow">
+            <div className="modal-body p-4 text-center">
+              <h5 className="mb-0">Oh non... 😓</h5>
+              <p className="mb-0">
+                Cette page est actuellement en cours de développement... 🚧
+              </p>
+            </div>
+            <div className="modal-footer flex-nowrap justify-content-center p-0">
+              <Link to="/products">
+                <button
+                  type="button"
+                  className="btn btn-lg btn-primary mt-3 mb-3"
+                >
+                  <strong>D'accord !</strong>
+                </button>
+              </Link>
+              <Link to="/">
+                <button
+                  type="button"
+                  className="btn btn-lg btn-secondary mt-3 mb-3"
+                  data-bs-dismiss="modal"
+                >
+                  Aller dans la page d'accueil
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
       </>
     );
   }
--- src/assets/components/tools/tools.tsx
+++ src/assets/components/tools/tools.tsx
@@ -5,6 +5,7 @@ import transitionTools from "./pages/transitionTools"
 import React from './images/react.png'
 import Angular from './images/angular.png'
 import Vue from './images/vue.png'
+import Ubuntu from './images/ubuntu.png'
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
 
@@ -18,7 +19,7 @@ function Tools() {
     <img className="me-3" src={Logo} alt="" width="48" height="38" />
     <div className="lh-1">
       <h1 className="h6 mb-0 text-white lh-1">Levetica</h1>
-      <small>v.1.3.0-beta-9</small>
+      <small>v.1.3.0-beta-10</small>
     </div>
   </div>
 
@@ -45,44 +46,42 @@ function Tools() {
       </p>
     </div>
     <small className="d-block text-end mt-3">
-      <a className="text-dark" href="#">All updates</a>
+      <a className="text-dark" href="#">Autre page</a>
     </small>
   </div>
 
   <div className="my-3 p-3 bg-success bg-opacity-25 rounded shadow-sm">
-    <h6 className="border-bottom pb-2 mb-0 text-start fw-bold">Suggestions</h6>
+    <h6 className="border-bottom pb-2 mb-0 text-start fw-bold">Tendances actuelles</h6>
     <div className="d-flex text-body-secondary pt-3">
-      <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-      <div className="pb-3 mb-0 small lh-sm border-bottom w-100">
+    <img src={Ubuntu} width={50} />
+    <div className="pb-3 mb-0 small lh-sm border-bottom w-100">
         <div className="d-flex justify-content-between">
-          <strong className="text-dark">Full Name</strong>
-          <a className="text-dark" href="#">Follow</a>
+          <strong className="text-dark">&nbsp;&nbsp;Ubuntu</strong>
         </div>
-        <span className="d-block text-start text-dark">@username</span>
+        <span className="d-block text-start text-dark">
+        &nbsp;&nbsp;Ubuntu est une distribution Linux gratuite et conviviale, dérivée de Debian, destinée aux ordinateurs personnels, serveurs et objets connectés.</span>
       </div>
     </div>
     <div className="d-flex text-body-secondary pt-3">
       <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
       <div className="pb-3 mb-0 small lh-sm border-bottom w-100">
         <div className="d-flex justify-content-between">
-          <strong className="text-dark">Full Name</strong>
-          <a className="text-dark" href="#">Follow</a>
+          <strong className="text-dark">Nom de l'app</strong>
         </div>
-        <span className="d-block text-start text-dark">@username</span>
+        <span className="d-block text-start text-dark">Bientôt !</span>
       </div>
     </div>
     <div className="d-flex text-body-secondary pt-3">
       <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
       <div className="pb-3 mb-0 small lh-sm border-bottom w-100">
         <div className="d-flex justify-content-between">
-          <strong className="text-dark">Full Name</strong>
-          <a className="text-dark" href="#">Follow</a>
+          <strong className="text-dark">Nom de l'app</strong>
         </div>
-        <span className="d-block text-start text-dark">@username</span>
+        <span className="d-block text-start text-dark">Bientôt !</span>
       </div>
     </div>
     <small className="d-block text-end mt-3">
-      <a className="text-dark" href="#">All suggestions</a>
+      <a className="text-dark" href="#">Autre page</a>
     </small>
   </div>
   </main>
